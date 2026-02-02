afc6de90131674ff3ecee36c576caef5001e46b5
Josh Serpas <jdserpas@gmail.com>
2026-02-01 20:59:48 -0500

    added implementation of part 2

--- day-3/solution.py
+++ day-3/solution.py
@@ -16,10 +16,31 @@ def solve(data):
             total += int(highestLeft + highestRight)
         print(total) 
 
+def solve2(data):
+    k = 12
+    batteries = []
+    for line in data:
+        # How many digits do we need to drop?
+        drop = len(line) - 12
+        stack = []
+        for num in list(line):
+            while(len(stack) > 0 and drop > 0 and stack[-1] < num):
+                stack.pop()
+                drop -= 1
+            stack.append(num)
+        if drop > 0:
+            stack = stack[0:-drop]
+        batteries.append(int(''.join(stack)))
+    print(sum(batteries))
+
 try:
     with open("example.txt", "r") as file:
         data = file.read().strip().splitlines()
+        print(">>>>>>>>>>>>>>>>>>>> EXAMPLE SOLUTION OUTPUT: >>>>>>>>>>>>>>>>>>>>>>>>>")
+        print(" PART 1: ") 
         solve(data)
+        print(" PART 2: ")
+        solve2(data)
 
 except FileNotFoundError:
     print("File not found. Please ensure 'example.txt' exists in the current directory.")
@@ -27,6 +48,10 @@ except FileNotFoundError:
 try:
     with open("input.txt", "r") as file:
         data = file.read().strip().splitlines()
+        print(">>>>>>>>>>>>>>>>>>>> SOLUTION OUTPUT: >>>>>>>>>>>>>>>>>>>>>>>>>")
+        print(" PART 1: ") 
         solve(data)
+        print(" PART 2: ")
+        solve2(data)
 except FileNotFoundError:
     print("File not found. Please ensure 'input.txt' exists in the current directory.")
