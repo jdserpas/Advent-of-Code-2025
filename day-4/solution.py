f9917a84cbe4acfbb0d9360c407419333aeb487f
Josh Serpas <jdserpas@gmail.com>
2026-02-12 19:11:21 -0500

    part 1 implementation

--- day-4/solution.py
+++ day-4/solution.py
@@ -1,7 +1,44 @@
 def solve(data):
-    # TODO
+    dataH = len(data)
+    dataW = len(data[0])
+
+    padded = [
+        [0] * (dataW + 2)
+        for _ in range(dataH + 2)
+    ]
+
+    for i in range(dataH):
+        for j in range(dataW):
+            padded[i + 1][j + 1] = 1 if data[i][j] == '@' else 0
+
+    count = 0
+
+    for i in range(1, dataH + 1):
+        for j in range(1, dataW + 1):
+            if padded[i][j] == 0:
+                continue
+            rolls = 0
+            # check 3 above
+            rolls += padded[i - 1][j - 1]
+            rolls += padded[i - 1][j]
+            rolls += padded[i - 1][j + 1]
+            # check 2 on row
+            rolls += padded[i][j - 1]
+            rolls += padded[i][j + 1]
+            #check 3 bellow
+            rolls += padded[i + 1][j - 1]
+            rolls += padded[i + 1][j]
+            rolls += padded[i + 1][j + 1]
+
+            if rolls < 4:
+                count+=1
+             
+
+    return count 
+    
 
 def solve2(data):
+    print("Solution 2 not implemented")
     # TODO
 
 def print_section(title, part1, part2):
